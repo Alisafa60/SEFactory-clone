839aea14d2a8014cd08e1576f1292d8316aa6e97
Alisafa60 <alisafa6100@gmail.com>
2023-11-16 02:06:40 +0200

    empty overlay menu and 3rd page done

--- script.js
+++ script.js
@@ -1,80 +1,101 @@
-
-const trainingTopText = document.getElementsByClassName("top-text-1");
-trainingTopText[0].innerText = "want to become a";
-let courses = document.getElementById("courses");
-const trainingDescription = document.getElementById("training-description");
-trainingDescription.innerText =
-"Jumps tart your journey in tech with our courses in Computer Science, Full-Stack Web development,\
- Data Engineering and UI/UX Design. Get hands-on experience and gain essential skills for a successful\
- tech career.";
-
-
-
-const phrases = ["Software Engineer?", "Data Engineer?", "UI/UX Designer?"];
-const speed = 100;
 const coursesContainer = document.getElementById("courses");
+const phrases = ["Software Engineer?", "Data Engineer?", "UI/UX Designer?"];
+const menuIcon = document.getElementById("menu-icon");
+const overlay = document.getElementById("overlay-menu");
+
 
 function animatePhrases(index) {
   if (index < phrases.length) {
     animatePhrase(phrases[index], function () {
-      // callback function to start the next phrase animation
       animatePhrases(index + 1);
     });
   } else {
     // reset index to 0 and start the animation loop again
     setTimeout(function () {
       animatePhrases(0);
-    }, 200); // Add a delay before restarting the animation
+    }, 200); 
   }
 }
 
 function animatePhrase(phrase, callback) {
-   let i = 0;
+  let i = 0;
 
-   function typeWriter() {
+  function typeWriter() {
     if (i < phrase.length) {
-          coursesContainer.innerHTML += phrase.charAt(i);
-          i++;
-          setTimeout(typeWriter, speed);
-        } else {
-          setTimeout(erasePhrase, 1250, callback);
-        }
-    }
-
-   function erasePhrase(callback) {
-    if (i >= 0) {
-        coursesContainer.innerHTML = phrase.substring(0, i - 1);
-        i--;
-        setTimeout(erasePhrase, 50, callback);
+      coursesContainer.innerHTML += phrase.charAt(i);
+      i++;
+      setTimeout(typeWriter, 100);
     } else {
-          // clear the container after erasing the phrase
-        coursesContainer.innerHTML = '';
-          // ctart the next phrase animation
-        callback();
+      setTimeout(function () {
+        erasePhrase(callback);
+      }, 1250);
     }
-   }
+  }
 
-   typeWriter();
+  function erasePhrase(callback) {
+    if (i >= 0) {
+      coursesContainer.innerHTML = phrase.substring(0, i - 1);
+      i--;
+      setTimeout(function () {
+        erasePhrase(callback);
+      }, 50);
+    } else {
+      coursesContainer.innerHTML = '';
+      callback();
+      changeColor();
+    }
+  }
+
+  typeWriter();
 }
 
-    // Start the animation when the page loads
+// start the animation when the page loads
 window.onload = function () {
-    animatePhrases(0);
+  animatePhrases(0);
 };
- 
+
 const colors = [
-    "rgba(51, 235, 168, 0.9)",
-    "rgba(157, 109, 219, 0.9)",
-    "rgba(248, 89, 147, 0.9)"
+  "rgba(157, 109, 219, 0.9)",
+  "rgba(248, 89, 147, 0.9)",
+  "rgba(51, 235, 168, 0.9)",
 ];
 
 let currentColorIndex = 0;
 
 function changeColor() {
-    const overlay = document.getElementById("colorChangeOverlay");
-    overlay.style.backgroundColor = colors[currentColorIndex];
+  const overlay = document.getElementById("color-change");
+  overlay.style.backgroundColor = colors[currentColorIndex];
 
-    currentColorIndex = (currentColorIndex + 1) % colors.length;
+  currentColorIndex = (currentColorIndex + 1) % colors.length;
 }
 
-setInterval(changeColor, 2400);
+menuIcon.addEventListener("click", function() {
+  overlay.style.display = "block";
+  setTimeout(() => {
+    overlay.style.opacity = 1;
+    addCloseButton();
+  }, 10);
+});
+
+function closeOverlay() {
+  overlay.style.opacity = 0;
+  setTimeout(() => {
+    overlay.style.display = "none";
+    removeCloseButton();
+  }, 500); 
+}
+
+function addCloseButton() {
+  const closeButton = document.createElement("button");
+  closeButton.textContent = "Close";
+  closeButton.addEventListener("click", closeOverlay);
+  document.getElementById("menu-content").appendChild(closeButton);
+}
+
+function removeCloseButton() {
+  const closeButton = document.querySelector("#menu-content button");
+  if (closeButton) {
+    closeButton.removeEventListener("click", closeOverlay);
+    closeButton.parentNode.removeChild(closeButton);
+  }
+}
